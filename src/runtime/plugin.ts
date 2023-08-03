50e91444344229a977f9d0e6422d9b89eb84cf8a
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-08-03 14:54:54 +0800

    feat: add sanctum-auth & sanctum-guest middleware

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,7 +1,12 @@
-import { defineNuxtPlugin } from "nuxt/app";
+import { addRouteMiddleware, defineNuxtPlugin } from "#imports";
 import { useSanctumAuth } from "./composables/useSanctumAuth";
+import sanctumAuth from "./middleware/sanctumAuth";
+import sanctumGuest from "./middleware/sanctumGuest";
 
 export default defineNuxtPlugin(async () => {
+  addRouteMiddleware("sanctum-auth", sanctumAuth);
+  addRouteMiddleware("sanctum-guest", sanctumGuest);
+
   if (process.server) return;
 
   const { isAuthenticated, fetchUser, loginToken, isInitialized } =
